9c88d144891c63aeba7758cab121463c6d1f6fa4
unknown <knaveenkumar2424@gmail.com>
2026-01-22 16:05:38 +0530

    third commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -59,6 +59,6 @@ exports.login = async (req, res) => {
          res.status(200).json({ message: "success login" ,token});
 
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        res.status(501).json({ message: "Internal server error" ,error});
     }
 }   
